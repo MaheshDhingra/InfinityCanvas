1e091590cb185412a030ad6c591383cd79bfb094
Mahesh <dhingram447@gmail.com>
2025-08-01 18:33:11 +0530

    v0.1.2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone', // Enables a standalone build for custom server
   /* config options here */
 };
 
